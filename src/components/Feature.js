cf0d0aa700b4b4330639643753029de6b2a74d55
Hamza Iqbal <hamzaiqbal7104@gmail.com>
2022-07-26 19:09:40 +0500

    css

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import FImg from '../assets/Feature-Img.svg';
-import view from '../assets/Vector (1).svg';
-import arrow from '../assets/Arrow.svg';
-
+import React from "react";
+import FImg from "../assets/Feature-Img.svg";
+import view from "../assets/Vector (1).svg";
+import arrow from "../assets/Arrow.svg";
 
 const Feature = () => {
   return (
@@ -11,36 +10,40 @@ const Feature = () => {
         <div className="feature-img">
           <img src={FImg} alt="Not found" />
         </div>
-        <div className='feature-deatil'>
-          <div className='row1'>
-            <div className='col1'>
+        <div className="feature-deatil">
+          <div className="row1">
+            <div className="col1">
               <p>Posted on October 6th 2021</p>
             </div>
-            <div className='col2'>
+            <div className="col2">
               <img src={view} alt="Not found" />
             </div>
-            <div className='col3'>
+            <div className="col3">
               <p>563 views</p>
             </div>
-            <div className='col4'>
+            <div className="col4">
               <p>Featured</p>
             </div>
           </div>
-          <div className='row2'>
-            <div className='col5'>
+          <div className="row2">
+            <div className="col5">
               <h4>Should I Buy a New Car or Lease a New Car in 2021?</h4>
             </div>
           </div>
-          <div className='row3'>
-            <div className='col6'>
-              <p>We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical.</p>
+          <div className="row3">
+            <div className="col6">
+              <p>
+                We provide a full range of front end mechanical repairs for all
+                makes and models of cars, no matter the cause. This includes, We
+                provide a full range of front end mechanical.
+              </p>
             </div>
           </div>
-          <div className='row4'>
-            <div className='col7'>
+          <div className="row4">
+            <div className="col7">
               <p>Read more</p>
             </div>
-            <div className='col8'>
+            <div className="col8">
               <a href="/blog" target="_blank">
                 <img src={arrow} alt="Not found" />
               </a>
@@ -49,6 +52,6 @@ const Feature = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Feature;
